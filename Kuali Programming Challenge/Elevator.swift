8a4d3ef1d4b9991a2d3c4b51cc51aa83348613dd
Todd Perkins <todd@example.com>
2018-05-29 14:35:52 -0700

    Added code to check if Elevator can handle a request.

--- Kuali Programming Challenge/Elevator.swift
+++ Kuali Programming Challenge/Elevator.swift
@@ -17,6 +17,8 @@ class Elevator : NSObject {
     var floorToStop:Int = 0 // if not zero, this is a floor to stop on
     var direction:Int = 1 // 1 = up, -1 = down
     var containsPassengers:Bool = false // whether the elevator currently contains passengers
+    var currentRequests:[ElevatorRequest] = [] // requests being handled by this elevator
+    var doorIsOpened:Bool = false
     
     init(num:Int){ // when we init, we only need the index since all elevators start at floor 1
         self.elevatorNum = num
@@ -25,15 +27,28 @@ class Elevator : NSObject {
         nextFloor()
     }
     
-    func didReceiveRequest(req:ElevatorRequest){
+    func canHandleRequest(req:ElevatorRequest) -> Bool {
+        if(!containsPassengers) { // elevator can always handle a request if it's empty
+            return true
+        }
+        let onTheWayUp:Bool = (req.destinationFloor > currentFloor && req.destinationFloor < floorToStop && direction == 1) // passenger is on the way when elevator is going that direction, and between current floor and where the elevator is going to stop anyway
+        let onTheWayDown:Bool = (req.destinationFloor < currentFloor && req.destinationFloor > floorToStop && direction == -1)
+        let passengerIsOnTheWay:Bool =  onTheWayUp || onTheWayDown
         
+        if(containsPassengers && passengerIsOnTheWay) {
+            return true
+        }
+        return false
+    }
+    
+    func didReceiveRequest(req:ElevatorRequest){
+        currentRequests.append(req)
     }
     
     @objc func move(){
         if(currentFloor >= maxFloor) {
             currentFloor = maxFloor
             //print("elevator \(elevatorNum) has reached the top")
-            return
         }
         if(currentFloor <= 1) {
             currentFloor = 1
@@ -41,11 +56,13 @@ class Elevator : NSObject {
         }
         currentFloor += direction
         print("elevator \(elevatorNum) is now at floor \(currentFloor)")
-        nextFloor()
+        if(currentRequests.count > 0) {
+            nextFloor()
+        }
     }
     
     func closeDoor() {
-        Timer.scheduledTimer(withTimeInterval: 0.1, repeats: false) { (timer:Timer) in
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { (timer:Timer) in
             print("elevator \(self.elevatorNum) closed its door")
             self.nextFloor()
         }
